986b1a0013b90bd4ad37efd94a5a8ab0ab72c60a
brightkirara@hanmail.net <brightkirara@hanmail.net@dc230538-0670-e9d8-06d9-70bcc412052d>
2010-10-07 17:50:46 +0000

    비디오 딜레이가 있는 영상을 자동적으로 딜레이를 0으로 만들어주는 스크립트 추가(비디오와 오디오 싱크 맞춤) 스크립트 부분 수정(M2TS 형식 파일은 DirectShowSource로 인코딩하도록 변경)

--- trunk/My Project/AssemblyInfo.vb
+++ trunk/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 지정되도록 할 수 있습니다.
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.0.0.3")> 
-<Assembly: AssemblyFileVersion("3.0.0.3")> 
+<Assembly: AssemblyVersion("3.0.0.4")> 
+<Assembly: AssemblyFileVersion("3.0.0.4")> 
