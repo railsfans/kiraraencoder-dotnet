2a48d7ab854a8c12b58bd186632019680c9309f1
brightkirara@gmail.com <brightkirara@gmail.com@dc230538-0670-e9d8-06d9-70bcc412052d>
2011-02-21 19:34:47 +0000

    seekmode 버그 수정 mplayer 업데이트

--- trunk/My Project/AssemblyInfo.vb
+++ trunk/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' 지정되도록 할 수 있습니다.
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.6.0.0")> 
-<Assembly: AssemblyFileVersion("3.6.0.0")> 
+<Assembly: AssemblyVersion("3.6.0.1")> 
+<Assembly: AssemblyFileVersion("3.6.0.1")> 
